a8bee61095e833ec5acc5b52b2424ed2a7cf747f
Codigo-Fuentes <murray.r.adam@gmail.com>
2022-11-03 14:12:02 +0100

    chore: add error boundary

--- pages/_app.js
+++ pages/_app.js
@@ -6,25 +6,28 @@ import { UploadsListProvider } from "@w3ui/react-uploads-list";
 import "../styles/globals.css";
 import "../styles/tachyons.min.css";
 import "../styles/spinner.css";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 function App({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <UploaderProvider>
-        <UploadsListProvider>
-          <IdentityLoader>
-            <div className="vh-100 pt5 flex flex-column justify-center items-center sans-serif light-silver">
-              <header>
-                <img src="/logo.png" width="250" alt="logo" />
-              </header>
-              <div className="w-90 w-50-ns mw6 h-100">
-                <Component {...pageProps} />
+    <ErrorBoundary>
+      <AuthProvider>
+        <UploaderProvider>
+          <UploadsListProvider>
+            <IdentityLoader>
+              <div className="vh-100 pt5 flex flex-column justify-center items-center sans-serif light-silver">
+                <header>
+                  <img src="/logo.png" width="250" alt="logo" />
+                </header>
+                <div className="w-90 w-50-ns mw6 h-100">
+                  <Component {...pageProps} />
+                </div>
               </div>
-            </div>
-          </IdentityLoader>
-        </UploadsListProvider>
-      </UploaderProvider>
-    </AuthProvider>
+            </IdentityLoader>
+          </UploadsListProvider>
+        </UploaderProvider>
+      </AuthProvider>
+    </ErrorBoundary>
   );
 }
 
